9e1bce4d7134199952a485ba2563ead554e46449
andrescrojas <ing-andresrojas@hotmail.com>
2017-05-15 10:09:34 -0500

    Update samples.go

--- temperature_simple_contract/samples.go
+++ temperature_simple_contract/samples.go
@@ -17,24 +17,347 @@ Contributors:
 Andres Rojas  - Assist Consultores - acrojas@assist.com.co
 ******************************************************************************/
 
+
 package main
 
-var samples = `
-{
-	"event": {
-		"name": "Dispositivo02",		
-		"temperature": 80,
-		"hora": "Wed May 10 00:12:14 2017",
-		"objectTemp": "25"
+var schemas = `{
+	"API": {
+		"createAsset": {
+			"description": "Create an asset. One argument, a JSON encoded event. name is required with zero or more writable properties. Establishes an initial asset state.",
+			"properties": {
+				"args": {
+					"description": "args are JSON encoded strings",
+					"items": {
+						"description": "A set of fields that constitute the writable fields in an asset's state. name is mandatory along with at least one writable field. In this contract pattern, a partial state is used as an event.",
+						"properties": {
+							"name": {
+								"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+								"type": "string"
+							},							
+							"temperature": {
+								"description": "Temperature of the asset in Fahrenheit.",								
+								"type": "number"
+							},
+							"hora": {
+								"description": "Datetime of the record.",
+								"type": "string"
+							},
+							"objectTemp": {
+								"description": "Temperature of the object",
+								"type": "number"
+							}
+						},
+						"required": [
+							"name"
+						],
+						"type": "object"
+					},
+					"maxItems": 1,
+					"minItems": 1,
+					"type": "array"
+				},
+				"function": {
+					"description": "createAsset function",
+					"enum": [
+						"createAsset"
+					],
+					"type": "string"
+				},
+				"method": "invoke"
+			},
+			"type": "object"
+		},
+		"deleteAsset": {
+			"description": "Delete an asset. Argument is a JSON encoded string containing only an name.",
+			"properties": {
+				"args": {
+					"description": "args are JSON encoded strings",
+					"items": {
+						"description": "An object containing only an name for use as an argument to read or delete.",
+						"properties": {
+							"name": {
+								"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+								"type": "string"
+							}
+						},
+						"type": "object"
+					},
+					"maxItems": 1,
+					"minItems": 1,
+					"type": "array"
+				},
+				"function": {
+					"description": "deleteAsset function",
+					"enum": [
+						"deleteAsset"
+					],
+					"type": "string"
+				},
+				"method": "invoke"
+			},
+			"type": "object"
+		},
+		"init": {
+			"description": "Initializes the contract when started, either by deployment or by peer restart.",
+			"properties": {
+				"args": {
+					"description": "args are JSON encoded strings",
+					"items": {
+						"description": "event sent to init on deployment",
+						"properties": {
+							"nickname": {
+								"default": "TEMPERATURE",
+								"description": "The nickname of the current contract",
+								"type": "string"
+							},
+							"version": {
+								"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+								"type": "string"
+							}
+						},
+						"required": [
+							"version"
+						],
+						"type": "object"
+					},
+					"maxItems": 1,
+					"minItems": 1,
+					"type": "array"
+				},
+				"function": {
+					"description": "init function",
+					"enum": [
+						"init"
+					],
+					"type": "string"
+				},
+				"method": "deploy"
+			},
+			"type": "object"
+		},
+		"readAsset": {
+			"description": "Returns the state an asset. Argument is a JSON encoded string. name is the only accepted property.",
+			"properties": {
+				"args": {
+					"description": "args are JSON encoded strings",
+					"items": {
+						"description": "An object containing only an name for use as an argument to read or delete.",
+						"properties": {
+							"name": {
+								"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+								"type": "string"
+							}
+						},
+						"type": "object"
+					},
+					"maxItems": 1,
+					"minItems": 1,
+					"type": "array"
+				},
+				"function": {
+					"description": "readAsset function",
+					"enum": [
+						"readAsset"
+					],
+					"type": "string"
+				},
+				"method": "query",
+				"result": {
+					"description": "A set of fields that constitute the complete asset state.",
+					"properties": {
+						"name": {
+							"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+							"type": "string"
+						},							
+						"temperature": {
+							"description": "Temperature of the asset in Fahrenheit.",								
+							"type": "number"
+						},
+						"hora": {
+							"description": "Datetime of the record.",
+							"type": "string"
+						},
+						"objectTemp": {
+							"description": "Temperature of the object",
+							"type": "number"
+						}
+					},
+					"type": "object"
+				}
+			},
+			"type": "object"
+		},
+		"readAssetSamples": {
+			"description": "Returns a string generated from the schema containing sample Objects as specified in generate.json in the scripts folder.",
+			"properties": {
+				"args": {
+					"description": "accepts no arguments",
+					"items": {},
+					"maxItems": 0,
+					"minItems": 0,
+					"type": "array"
+				},
+				"function": {
+					"description": "readAssetSamples function",
+					"enum": [
+						"readAssetSamples"
+					],
+					"type": "string"
+				},
+				"method": "query",
+				"result": {
+					"description": "JSON encoded object containing selected sample data",
+					"type": "string"
+				}
+			},
+			"type": "object"
+		},
+		"readAssetSchemas": {
+			"description": "Returns a string generated from the schema containing APIs and Objects as specified in generate.json in the scripts folder.",
+			"properties": {
+				"args": {
+					"description": "accepts no arguments",
+					"items": {},
+					"maxItems": 0,
+					"minItems": 0,
+					"type": "array"
+				},
+				"function": {
+					"description": "readAssetSchemas function",
+					"enum": [
+						"readAssetSchemas"
+					],
+					"type": "string"
+				},
+				"method": "query",
+				"result": {
+					"description": "JSON encoded object containing selected schemas",
+					"type": "string"
+				}
+			},
+			"type": "object"
+		},
+		"updateAsset": {
+			"description": "Update the state of an asset. The one argument is a JSON encoded event. name is required along with one or more writable properties. Establishes the next asset state. ",
+			"properties": {
+				"args": {
+					"description": "args are JSON encoded strings",
+					"items": {
+						"description": "A set of fields that constitute the writable fields in an asset's state. name is mandatory along with at least one writable field. In this contract pattern, a partial state is used as an event.",
+						"properties": {
+							"name": {
+								"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+								"type": "string"
+							},							
+							"temperature": {
+								"description": "Temperature of the asset in Fahrenheit.",								
+								"type": "number"
+							},
+							"hora": {
+								"description": "Datetime of the record.",
+								"type": "string"
+							},
+							"objectTemp": {
+								"description": "Temperature of the object",
+								"type": "number"
+							}
+						},
+						"required": [
+							"name"
+						],
+						"type": "object"
+					},
+					"maxItems": 1,
+					"minItems": 1,
+					"type": "array"
+				},
+				"function": {
+					"description": "updateAsset function",
+					"enum": [
+						"updateAsset"
+					],
+					"type": "string"
+				},
+				"method": "invoke"
+			},
+			"type": "object"
+		}
 	},
-	"initEvent": {
-		"nickname": "TEMPERATURE",
-		"version": "2"
-	},
-	"state": {
-		"name": "Dispositivo02",		
-		"temperature": 85,
-		"hora": "Wed May 10 00:12:14 2017",
-		"objectTemp": "25"
+	"objectModelSchemas": {
+		"deviceIDKey": {
+			"description": "An object containing only an name for use as an argument to read or delete.",
+			"properties": {
+				"name": {
+					"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+					"type": "string"
+				}
+			},
+			"type": "object"
+		},
+		"event": {
+			"description": "A set of fields that constitute the writable fields in an asset's state. name is mandatory along with at least one writable field. In this contract pattern, a partial state is used as an event.",
+			"properties": {
+				"name": {
+					"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+					"type": "string"
+				},							
+				"temperature": {
+					"description": "Temperature of the asset in Fahrenheit.",								
+					"type": "number"
+				},
+				"hora": {
+					"description": "Datetime of the record.",
+					"type": "string"
+				},
+				"objectTemp": {
+					"description": "Temperature of the object",
+					"type": "number"
+				}
+			},
+			"required": [
+				"name"
+			],
+			"type": "object"
+		},
+		"initEvent": {
+			"description": "event sent to init on deployment",
+			"properties": {
+				"nickname": {
+					"default": "TEMPERATURE",
+					"description": "The nickname of the current contract",
+					"type": "string"
+				},
+				"version": {
+					"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+					"type": "string"
+				}
+			},
+			"required": [
+				"version"
+			],
+			"type": "object"
+		},
+		"state": {
+			"description": "A set of fields that constitute the complete asset state.",
+			"properties": {
+				"name": {
+					"description": "The ID of a managed asset. The resource focal point for a smart contract.",
+					"type": "string"
+				},							
+				"temperature": {
+					"description": "Temperature of the asset in Fahrenheit.",								
+					"type": "number"
+				},
+				"hora": {
+					"description": "Datetime of the record.",
+					"type": "string"
+				},
+				"objectTemp": {
+					"description": "Temperature of the object",
+					"type": "number"
+				}
+			},
+			"type": "object"
+		}
 	}
 }`
